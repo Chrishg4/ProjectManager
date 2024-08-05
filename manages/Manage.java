085176660dd1cb882137fdebbd41f9b917db85eb
Kristy <Kralvardogu@est.utn.ac.cr>
2024-08-05 16:18:43 -0600

    Arreglo

--- manages/Manage.java
+++ manages/Manage.java
@@ -4,10 +4,31 @@
  */
 package manages;
 
+import Projects.Project;
+import Users.User;
+import java.util.ArrayList;
+
 /**
  *
  * @author krist
  */
 public class Manage {
+    private ArrayList<Project> projects; 
+    private ArrayList<User> users;
+
+    public ArrayList<Project> getProjects() {
+        return projects;
+    }
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }   
+
+    public Manage(ArrayList<Project> projects, ArrayList<User> users) {
+        this.projects = projects;
+        this.users = users;
+    }
+    
+    
     
 }
