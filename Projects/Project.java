ad9d95cacecf497d1be9957a99954a1d37b2595b
Kristy <Kralvardogu@est.utn.ac.cr>
2024-08-05 14:03:10 -0600

    Clase project
    Se creo la clase project
    
    Atributos y metodos

---
+++ Projects/Project.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Projects;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author krist
+ */
+public class Project {
+    private int id;
+    private String nombre;
+    private ArrayList<Task> Task;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public ArrayList<Task> getTask() {
+        return Task;
+    }
+    
+    public void add(Task Task){
+        this.Task.add(Task);
+    }
+
+    public Project(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+        this.Task = new ArrayList();
+    }
+
+    public Project() {
+        this(0,"");
+    }
+    
+    
+    
+    
+}
